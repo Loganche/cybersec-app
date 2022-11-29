3cd14f1b8ad49a4ca447ac74ce4f53240ddbc913
Alexander Loginov <lxndrloginov@gmail.com>
2022-11-29 15:43:39 +0300

    Fixed fuzzing

--- tests/fuzzer.py
+++ tests/fuzzer.py
@@ -2,13 +2,7 @@ import sys
 
 import atheris
 
-with atheris.instrument_imports():
-    import cybersec
+atheris.instrument_all()
 
-
-def TestOneInput(data):
-    cybersec.parse(data)
-
-
-atheris.Setup(sys.argv, TestOneInput)
+atheris.Setup(sys.argv)
 atheris.Fuzz()
