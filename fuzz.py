3cae7e83962e978b70901ccdde1965bed32477f6
Alexander Loginov <lxndrloginov@gmail.com>
2023-03-06 11:25:19 +0300

    tuned fuzzing

--- fuzz.py
+++ fuzz.py
@@ -5,11 +5,12 @@ from requests import post
 @PythonFuzz
 def fuzz(buf):
     try:
-        string = buf.decode('ascii')
+        string = buf.decode(errors='replace')
         form = {'username': string[:len(string) // 2], 'password': string[len(string) // 2:]}
         post(url='http://127.0.0.1:8000/auth/token', data=form)
-    except Exception:
-        pass
+    except Exception as e:
+        print(e.args)
+        raise Exception("fuzzing stopped")
 
 
 if __name__ == '__main__':
